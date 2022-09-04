c9daa09c9c4bdca8d02d760bda927cd397b93e6a
DavidDworetzky <djdworetz@gmail.com>
2022-09-04 17:15:31 -0500

    Add patch to simple diffusion script.

--- examples/simple_diffusion.py
+++ examples/simple_diffusion.py
@@ -2,6 +2,20 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 
+from diffusers.models.attention import BasicTransformerBlock
+
+
+from fastcore.basics import patch
+
+#Patching for Mac M1 
+@patch
+def forward(self:BasicTransformerBlock, x, context=None):
+    x = self.attn1(self.norm1(x.contiguous())) + x # <--- added x.contiguous()
+    x = self.attn2(self.norm2(x), context=context) + x
+    x = self.ff(self.norm3(x)) + x
+    return x
+
+
 model_id = "CompVis/stable-diffusion-v1-4"
 device = "cuda"
 
