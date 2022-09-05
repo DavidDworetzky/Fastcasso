911ad3b49d72bcf19b515ecb07882d55cbc32a17
DavidDworetzky <djdworetz@gmail.com>
2022-09-05 00:56:55 -0500

    Basic pipeline monkeypatching added.

--- examples/simple_diffusion.py
+++ examples/simple_diffusion.py
@@ -2,9 +2,29 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 
+import numpy as np
+import torch
+import torch.nn as nn
+
+from transformers import CLIPConfig
+
+flag_safety = True
+class StableDiffusionTrivialSafetyChecker(object):
+    config_class = CLIPConfig
+
+    def __init__(self):
+        print('Safety Checker Initialized.')
+
+    @torch.no_grad()
+    def forward(self, clip_input, images):
+        return images, False
+
+    def safety_check(self, clip_input, images):
+        return self.forward(clip_input, images)
+
+safety_checker = StableDiffusionTrivialSafetyChecker()
+
 from diffusers.models.attention import BasicTransformerBlock
-
-
 from fastcore.basics import patch
 
 #Patching for Mac M1 
@@ -21,10 +41,12 @@ device = "mps"
 
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
+if not flag_safety:
+    pipe.safety_checker = safety_checker.safety_check
 pipe = pipe.to(device)
 
-prompt = "a photo of an astronaut riding a horse on mars"
+prompt = "An astronaut riding a horse on mars."
 with autocast("cpu"):
     image = pipe(prompt, guidance_scale=7.5)["sample"][0]  
     
-image.save("astronaut_rides_horse.png")
+image.save("output.png")
