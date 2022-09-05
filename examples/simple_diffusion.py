21e309b4d58dda0b796c8e54ceabe3427930a015
DavidDworetzky <djdworetz@gmail.com>
2022-09-05 00:07:54 -0500

    Finish Mac version of stable diffusion output.

--- examples/simple_diffusion.py
+++ examples/simple_diffusion.py
@@ -24,7 +24,7 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
 pipe = pipe.to(device)
 
 prompt = "a photo of an astronaut riding a horse on mars"
-with autocast("cuda"):
+with autocast("cpu"):
     image = pipe(prompt, guidance_scale=7.5)["sample"][0]  
     
 image.save("astronaut_rides_horse.png")
