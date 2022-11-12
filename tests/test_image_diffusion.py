413a2d159c5129895b55417b344c4f5995d57e63
DavidDworetzky <djdworetz@gmail.com>
2022-11-12 10:54:15 -0600

    Add basic test for test image diffusion.

--- tests/test_image_diffusion.py
+++ tests/test_image_diffusion.py
@@ -2,16 +2,29 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from fastapi.testclient import TestClient
+from PIL import Image
 #import mocks for testing
-from unittest.mock import patch
+from unittest import mock
 
 from ..main import app
 
 client = TestClient(app)
 
+#Create Mock all white PNG
+def mock_generate_image_diffusion():
+    img = Image.new("RGB", (800, 1280), (255, 255, 255))
+    return img
 
 def test_read_version():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json().keys() == {"Version"}
+
+@mock.patch("app.mediators.image_diffusion.generate_image_diffusion")
+def test_generate_image_endpoint(
+    mock_generate_image_diffusion,
+):
+    mock_generate_image_diffusion.return_value = mock_generate_image_diffusion()
+    response = client.get("/image/generate/this%20is%20a%20test/test.png")
+    assert response.status_code == 200
 
