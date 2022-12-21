c37eec4e77dfd3a5ecea24b91ad13742001bfeb9
DavidDworetzky <djdworetz@gmail.com>
2022-12-21 15:19:16 -0600

    Add proper database connection from env.py

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from app.models.database.database import SQLALCHEMY_DATABASE_URL
 
 from alembic import context
 
@@ -38,7 +39,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = SQLALCHEMY_DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
