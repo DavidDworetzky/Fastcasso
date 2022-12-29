09154bdc1de13c6c4c38fe2ba11b1b55e082d059
DavidDworetzky <djdworetz@gmail.com>
2022-12-29 00:44:58 -0600

    Running experiment with textual inversion.

--- scripts/run_inversion.bat
+++ scripts/run_inversion.bat
@@ -1,12 +1,13 @@
 set MODEL_NAME="runwayml/stable-diffusion-v1-5"
-set DATA_DIR="path-to-dir-containing-images"
+set DATA_DIR="E:\AI_Experiment\Remy_Experiment"
 set TOKEN_NAME="<dog-remy>"
+set CLASS_NAME="dog"
 
 accelerate launch textual_inversion.py ^
---pretrained_model_name_or_path=$MODEL_NAME ^
---train_data_dir=$DATA_DIR ^
+--pretrained_model_name_or_path=%MODEL_NAME% ^
+--train_data_dir=%DATA_DIR% ^
 --learnable_property="object" ^
---placeholder_token="<dog-remy>" --initializer_token="dog" ^
+--placeholder_token="%TOKEN_NAME%" --initializer_token="%CLASS_NAME%" ^
 --resolution=512 ^
 --train_batch_size=1 ^
 --gradient_accumulation_steps=4 ^
@@ -14,4 +15,4 @@ accelerate launch textual_inversion.py ^
 --learning_rate=5.0e-04 --scale_lr ^
 --lr_scheduler="constant" ^
 --lr_warmup_steps=0 ^
---output_dir="textual_inversion_dog"
+--output_dir="%DATA_DIR%"
