3780ee63343e81ef525b913ae4a6aeed1f6fe2be
DavidDworetzky <djdworetz@gmail.com>
2022-12-29 00:55:43 -0600

    Add run inversion changes to textual inversion branch.

--- scripts/run_inversion.bat
+++ scripts/run_inversion.bat
@@ -2,12 +2,13 @@ set MODEL_NAME="runwayml/stable-diffusion-v1-5"
 set DATA_DIR="E:\AI_Experiment\Remy_Experiment"
 set TOKEN_NAME="<dog-remy>"
 set CLASS_NAME="dog"
+set DIFFUSERS_REPO_PATH ="E:\repos\diffusers\"
 
-accelerate launch textual_inversion.py ^
+accelerate launch %DIFFUSERS_REPO_PATH%textual_inversion.py ^
 --pretrained_model_name_or_path=%MODEL_NAME% ^
 --train_data_dir=%DATA_DIR% ^
 --learnable_property="object" ^
---placeholder_token="%TOKEN_NAME%" --initializer_token="%CLASS_NAME%" ^
+--placeholder_token=%TOKEN_NAME% --initializer_token=%CLASS_NAME% ^
 --resolution=512 ^
 --train_batch_size=1 ^
 --gradient_accumulation_steps=4 ^
@@ -15,4 +16,4 @@ accelerate launch textual_inversion.py ^
 --learning_rate=5.0e-04 --scale_lr ^
 --lr_scheduler="constant" ^
 --lr_warmup_steps=0 ^
---output_dir="%DATA_DIR%"
+--output_dir=%DATA_DIR%
