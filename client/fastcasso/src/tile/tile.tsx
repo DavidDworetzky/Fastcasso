3475cad3e9556931df1be2f706168c2dacf61299
DavidDworetzky <djdworetz@gmail.com>
2023-01-20 23:59:57 -0600

    Add tile to fastcasso client.

---
+++ client/fastcasso/src/tile/tile.tsx
@@ -0,0 +1,16 @@
+/*tile component. Given base 64 image data, it will render the image. */
+function tile (props: TileProperties) {
+    return (
+        <div style={{width: props.width ?? '400px', height: props.height ?? '400px'}}>
+            <img src={props.image} alt={props.alt}/>
+        </div>
+    );
+}
+
+/* props */
+interface TileProperties {
+    image: string;
+    alt?: string;
+    width?: number;
+    height?: number;
+}
